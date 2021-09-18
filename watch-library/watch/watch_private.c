d97e74058f8b6a1c69d1da1c08176cb52ab2db24
Joey Castillo <jose.castillo@gmail.com>
2021-09-18 13:12:10 -0400

    fix USB VID/PID

--- watch-library/watch/watch_private.c
+++ watch-library/watch/watch_private.c
@@ -255,18 +255,6 @@ void TC0_Handler(void) {
  *
  */
 
-#include "tusb.h"
-
-/* A combination of interfaces must have a unique product id, since PC will save device driver after the first plug.
- * Same VID/PID with different interface e.g MSC (first), then CDC (later) will possibly cause system error on PC.
- *
- * Auto ProductID layout's Bitmap:
- *   [MSB]         HID | MSC | CDC          [LSB]
- */
-#define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
-#define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
-                           _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
-
 //--------------------------------------------------------------------+
 // Device Descriptors
 //--------------------------------------------------------------------+
@@ -284,8 +272,8 @@ tusb_desc_device_t const desc_device =
 
     .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
 
-    .idVendor           = 0xCafe,
-    .idProduct          = USB_PID,
+    .idVendor           = 0x1209,
+    .idProduct          = 0x2151,
     .bcdDevice          = 0x0100,
 
     .iManufacturer      = 0x01,
