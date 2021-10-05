12ee9221354c863bddb21a076dd3e3bdface3c67
Joey Castillo <jose.castillo@gmail.com>
2021-10-05 18:01:26 -0400

    remove fake / placeholder widgets

--- launcher/launcher_config.h
+++ launcher/launcher_config.h
@@ -5,8 +5,6 @@
 #include "preferences_widget.h"
 #include "set_time_widget.h"
 #include "pulseometer_widget.h"
-#include "fake_widget_1.h"
-#include "fake_widget_2.h"
 
 #define LAUNCHER_NUM_WIDGETS 3
 
