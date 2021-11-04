b0ec40ae460063f711cac963b2e7a0d826c710d7
Wesley Ellis <tahnok@gmail.com>
2021-11-03 21:10:10 -0400

    Add stopwatch face to movement
    Simple stopwatch that only counts seconds (not subseconds), minutes
    and yes, even hours

---
+++ movement/watch_faces/complications/stopwatch_face.h
@@ -0,0 +1,26 @@
+#ifndef STOPWATCH_FACE_H_
+#define STOPWATCH_FACE_H_
+
+#include "movement.h"
+
+typedef struct {
+    bool running;
+    uint8_t seconds;
+    uint8_t minutes;
+    uint8_t hours;
+} stopwatch_state_t;
+
+void stopwatch_face_setup(movement_settings_t *settings, void ** context_ptr);
+void stopwatch_face_activate(movement_settings_t *settings, void *context);
+bool stopwatch_face_loop(movement_event_t event, movement_settings_t *settings, void *context);
+void stopwatch_face_resign(movement_settings_t *settings, void *context);
+
+static const watch_face_t stopwatch_face = {
+    stopwatch_face_setup,
+    stopwatch_face_activate,
+    stopwatch_face_loop,
+    stopwatch_face_resign,
+    NULL
+};
+
+#endif // STOPWATCH_FACE_H_
