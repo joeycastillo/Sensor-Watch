eb66b67e8ea37f841105c31243c641a7382465d2
Joey Castillo <joeycastillo@utexas.edu>
2021-12-05 16:24:17 -0600

    more accelerometer tests: track interrupts on each axis, log on the quarter hour

--- movement/watch_faces/demos/lis2dh_logging_face.c
+++ movement/watch_faces/demos/lis2dh_logging_face.c
@@ -12,14 +12,78 @@
 // Pressing the alarm button enters the log mode, where the main display shows the number of interrupts detected in each of the last
 // 24 hours (the hour is shown in the top right digit and AM/PM indicator, if the clock is set to 12 hour mode)
 
+void _lis2dh_logging_face_update_display(movement_settings_t *settings, lis2dh_logger_state_t *logger_state, lis2dh_interrupt_state interrupt_state, watch_date_time date_time) {
+    char buf[14];
+    char time_indication_character;
+    int8_t pos;
+
+    if (logger_state->log_ticks) {
+        pos = (logger_state->data_points - 1 - logger_state->display_index) % LIS2DH_LOGGING_NUM_DATA_POINTS;
+        if (pos < 0) {
+            watch_clear_colon();
+            sprintf(buf, "NO   data ");
+        } else {
+            date_time = logger_state->data[pos].timestamp;
+            watch_set_colon();
+            if (settings->bit.clock_mode_24h) {
+                watch_set_indicator(WATCH_INDICATOR_24H);
+            } else {
+                if (date_time.unit.hour > 11) watch_set_indicator(WATCH_INDICATOR_PM);
+                date_time.unit.hour %= 12;
+                if (date_time.unit.hour == 0) date_time.unit.hour = 12;
+            }
+            switch (logger_state->axis_index) {
+                case 0:
+                    sprintf(buf, "3A%2d%02d%4ld", date_time.unit.hour, date_time.unit.minute, logger_state->data[pos].x_interrupts + logger_state->data[pos].y_interrupts + logger_state->data[pos].z_interrupts);
+                    break;
+                case 1:
+                    sprintf(buf, "XA%2d%02d%4ld", date_time.unit.hour, date_time.unit.minute, logger_state->data[pos].x_interrupts);
+                    break;
+                case 2:
+                    sprintf(buf, "YA%2d%02d%4ld", date_time.unit.hour, date_time.unit.minute, logger_state->data[pos].y_interrupts);
+                    break;
+                case 3:
+                    sprintf(buf, "ZA%2d%02d%4ld", date_time.unit.hour, date_time.unit.minute, logger_state->data[pos].z_interrupts);
+                    break;
+            }
+        }
+    } else {
+        date_time = watch_rtc_get_date_time();
+        watch_clear_colon();
+        watch_clear_indicator(WATCH_INDICATOR_PM);
+        watch_clear_indicator(WATCH_INDICATOR_24H);
+        if ((59 - date_time.unit.second) < 10) time_indication_character = '0' + (59 - date_time.unit.second);
+        else time_indication_character = (date_time.unit.second % 2) ? 'i' : '_';
+        sprintf(buf, "%c%c%c%c%2d%2d%2d",
+            (interrupt_state & LIS2DH_INTERRUPT_STATE_Y_HIGH) ? 'Y' : ' ',
+            (interrupt_state & LIS2DH_INTERRUPT_STATE_X_HIGH) ? 'X' : ' ',
+            (interrupt_state & LIS2DH_INTERRUPT_STATE_Z_HIGH) ? 'Z' : ' ',
+            time_indication_character,
+            logger_state->interrupts[0],
+            logger_state->interrupts[1],
+            logger_state->interrupts[2]);
+    }
+    watch_display_string(buf, 0);
+}
+
 void _lis2dh_logging_face_log_data(lis2dh_logger_state_t *logger_state) {
     watch_date_time date_time = watch_rtc_get_date_time();
-    date_time.unit.hour = (date_time.unit.hour + 23) % 24; // log this as the number of events in the previous hour
+    // we get this call 15 minutes late; i.e. at 6:15 we're logging events for 6:00.
+    // so: if we're at the top of the hour, roll the hour back too (7:00 task logs data for 6:45)
+    if (date_time.unit.minute == 0) date_time.unit.hour = (date_time.unit.hour + 23) % 24;
+
+    // // then roll the minute back.
+    date_time.unit.minute = (date_time.unit.minute + 45) % 60;
+
     size_t pos = logger_state->data_points % LIS2DH_LOGGING_NUM_DATA_POINTS;
     logger_state->data[pos].timestamp.reg = date_time.reg;
-    logger_state->data[pos].interrupts = logger_state->interrupts_this_hour;
+    logger_state->data[pos].x_interrupts = logger_state->x_interrupts_this_hour;
+    logger_state->data[pos].y_interrupts = logger_state->y_interrupts_this_hour;
+    logger_state->data[pos].z_interrupts = logger_state->z_interrupts_this_hour;
     logger_state->data_points++;
-    logger_state->interrupts_this_hour = 0;
+    logger_state->x_interrupts_this_hour = 0;
+    logger_state->y_interrupts_this_hour = 0;
+    logger_state->z_interrupts_this_hour = 0;
 }
 
 void lis2dh_logging_face_setup(movement_settings_t *settings, void ** context_ptr) {
@@ -49,74 +113,47 @@ void lis2dh_logging_face_activate(movement_settings_t *settings, void *context) 
     watch_enable_digital_input(A1);
 }
 
-bool tick = false;
-
 bool lis2dh_logging_face_loop(movement_event_t event, movement_settings_t *settings, void *context) {
     lis2dh_logger_state_t *logger_state = (lis2dh_logger_state_t *)context;
     lis2dh_interrupt_state interrupt_state = 0;
     watch_date_time date_time;
-    char buf[14];
-    char time_indication_character;
-    int8_t pos;
 
     switch (event.event_type) {
         case EVENT_MODE_BUTTON_UP:
             movement_move_to_next_face();
             break;
         case EVENT_LIGHT_LONG_PRESS:
+            movement_illuminate_led();
             break;
         case EVENT_LIGHT_BUTTON_DOWN:
+            logger_state->axis_index = (logger_state->axis_index + 1) % 4;
+            logger_state->log_ticks = 255;
+            _lis2dh_logging_face_update_display(settings, logger_state, interrupt_state, date_time);
             break;
         case EVENT_ALARM_BUTTON_UP:
-            if (logger_state->log_ticks) {
-                logger_state->display_index = (logger_state->display_index + 1) % LIS2DH_LOGGING_NUM_DATA_POINTS;
-            }
-            logger_state->log_ticks = 60;
-            // fall through
+            if (logger_state->log_ticks) logger_state->display_index = (logger_state->display_index + 1) % LIS2DH_LOGGING_NUM_DATA_POINTS;
+            logger_state->log_ticks = 255;
+            logger_state->axis_index = 0;
+            _lis2dh_logging_face_update_display(settings, logger_state, interrupt_state, date_time);
+            break;
         case EVENT_ACTIVATE:
         case EVENT_TICK:
-            tick = !tick;
+            if (logger_state->log_ticks > 0) {
+                logger_state->log_ticks--;
+            } else {
+                logger_state->display_index = 0;
+            }
             if (watch_get_pin_level(A1)) {
                 watch_set_indicator(WATCH_INDICATOR_SIGNAL);
                 interrupt_state = lis2dh_get_int1_state();
                 logger_state->interrupts[0]++;
-                logger_state->interrupts_this_hour++;
+                if (interrupt_state & LIS2DH_INTERRUPT_STATE_X_HIGH) logger_state->x_interrupts_this_hour++;
+                if (interrupt_state & LIS2DH_INTERRUPT_STATE_Y_HIGH) logger_state->y_interrupts_this_hour++;
+                if (interrupt_state & LIS2DH_INTERRUPT_STATE_Z_HIGH) logger_state->z_interrupts_this_hour++;
             } else {
                 watch_clear_indicator(WATCH_INDICATOR_SIGNAL);
             }
-            if (logger_state->log_ticks) {
-                pos = (logger_state->data_points - 1 - logger_state->display_index) % LIS2DH_LOGGING_NUM_DATA_POINTS;
-                if (pos < 0) {
-                    watch_clear_colon();
-                    sprintf(buf, "NO   data ");
-                } else {
-                    date_time = logger_state->data[pos].timestamp;
-                    watch_set_colon();
-                    if (settings->bit.clock_mode_24h) {
-                        watch_set_indicator(WATCH_INDICATOR_24H);
-                    } else {
-                        if (date_time.unit.hour > 11) watch_set_indicator(WATCH_INDICATOR_PM);
-                        date_time.unit.hour %= 12;
-                        if (date_time.unit.hour == 0) date_time.unit.hour = 12;
-                    }
-                    sprintf(buf, "AT%2d1n%4ld", date_time.unit.hour, logger_state->data[pos].interrupts);
-                }
-            } else {
-                date_time = watch_rtc_get_date_time();
-                watch_clear_colon();
-                if ((59 - date_time.unit.second) < 10) time_indication_character = '0' + (59 - date_time.unit.second);
-                else time_indication_character = (date_time.unit.second % 2) ? 'i' : '_';
-                sprintf(buf, "%c%c%c%c%2d%2d%2d",
-                    (interrupt_state & LIS2DH_INTERRUPT_STATE_Y_HIGH) ? 'Y' : ' ',
-                    (interrupt_state & LIS2DH_INTERRUPT_STATE_X_HIGH) ? 'X' : ' ',
-                    (interrupt_state & LIS2DH_INTERRUPT_STATE_Z_HIGH) ? '2' : ' ',
-                    time_indication_character,
-                    logger_state->interrupts[0],
-                    logger_state->interrupts[1],
-                    logger_state->interrupts[2]);
-            }
-
-            watch_display_string(buf, 0);
+            _lis2dh_logging_face_update_display(settings, logger_state, interrupt_state, date_time);
             break;
         case EVENT_BACKGROUND_TASK:
             _lis2dh_logging_face_log_data(logger_state);
@@ -137,12 +174,13 @@ void lis2dh_logging_face_resign(movement_settings_t *settings, void *context) {
 bool lis2dh_logging_face_wants_background_task(movement_settings_t *settings, void *context) {
     (void) settings;
     lis2dh_logger_state_t *logger_state = (lis2dh_logger_state_t *)context;
+    watch_date_time date_time = watch_rtc_get_date_time();
 
     // this is kind of an abuse of the API, but, let's use the 1 minute tick to shift all our data over.
     logger_state->interrupts[2] = logger_state->interrupts[1];
     logger_state->interrupts[1] = logger_state->interrupts[0];
     logger_state->interrupts[0] = 0;
 
-    // and do our logging task at the top of the hour
-    return watch_rtc_get_date_time().unit.minute == 0;
+    // and do our logging task every 15 minutes
+    return (date_time.unit.minute % 15) == 0;
 }
