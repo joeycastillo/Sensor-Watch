34c7ecfd96f543a73af664004eaf16d92dfe90ed
Joey Castillo <joeycastillo@utexas.edu>
2022-01-25 17:06:04 -0500

    movement: move watch face includes to a separate file

--- movement/movement_faces.h
+++ movement/movement_faces.h
@@ -22,19 +22,27 @@
  * SOFTWARE.
  */
 
-#ifndef MOVEMENT_CONFIG_H_
-#define MOVEMENT_CONFIG_H_
+#ifndef MOVEMENT_FACES_H_
+#define MOVEMENT_FACES_H_
 
-#include "movement_faces.h"
+#include "simple_clock_face.h"
+#include "world_clock_face.h"
+#include "preferences_face.h"
+#include "set_time_face.h"
+#include "pulsometer_face.h"
+#include "thermistor_readout_face.h"
+#include "thermistor_logging_face.h"
+#include "character_set_face.h"
+#include "beats_face.h"
+#include "day_one_face.h"
+#include "voltage_face.h"
+#include "stopwatch_face.h"
+#include "totp_face.h"
+#include "lis2dh_logging_face.h"
+#include "demo_face.h"
+#include "hello_there_face.h"
+#include "sunrise_sunset_face.h"
+#include "countdown_face.h"
+#include "blinky_face.h"
 
-const watch_face_t watch_faces[] = {
-    simple_clock_face,
-    beats_face,
-    voltage_face,
-    preferences_face,
-    set_time_face,
-};
-
-#define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
-
-#endif // MOVEMENT_CONFIG_H_
+#endif // MOVEMENT_FACES_H_
