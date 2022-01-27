bbf146166834b91cf72d36d81b9d5d1145dbc6f2
joeycastillo <joeycastillo@users.noreply.github.com>
2022-01-27 15:51:46 +0000

    deploy: 14e4562b7a44ab6882180b69145665d4920769ac

--- search/all_9.js
+++ search/all_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['uart_110',['UART',['../group__debug.html',1,'']]],
+  ['uart_110',['UART',['../group__uart.html',1,'']]],
   ['utility_20functions_111',['Utility Functions',['../group__utility.html',1,'']]]
 ];
--- search/groups_7.js
+++ search/groups_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['uart_420',['UART',['../group__debug.html',1,'']]],
+  ['uart_420',['UART',['../group__uart.html',1,'']]],
   ['utility_20functions_421',['Utility Functions',['../group__utility.html',1,'']]]
 ];
