e2561cdff13cafa9d1bec75a4257b8c1dbeb48a6
joeycastillo <joeycastillo@users.noreply.github.com>
2022-02-07 15:20:15 +0000

    deploy: 79b037b4fb0a3f401406875c281afe2cf0864830

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['watch_5fdate_5ftime_0',['watch_date_time',['../unionwatch__date__time.html',1,'']]]
+  ['watch_5fdate_5ftime_0',['watch_date_time',['../unionwatch__date__time.html',1,'']]],
+  ['watch_5fduration_5ft_1',['watch_duration_t',['../structwatch__duration__t.html',1,'']]]
 ];
