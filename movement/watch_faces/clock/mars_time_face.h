3025984eb558442e86edf0c99f5d7fbb0d619f10
Joey Castillo <joeycastillo@utexas.edu>
2022-03-04 14:44:42 -0600

    movement: first pass at mars clock

--- movement/watch_faces/clock/mars_time_face.h
+++ movement/watch_faces/clock/mars_time_face.h
@@ -22,30 +22,27 @@
  * SOFTWARE.
  */
 
-#ifndef MOVEMENT_FACES_H_
-#define MOVEMENT_FACES_H_
+#ifndef MARS_TIME_FACE_H_
+#define MARS_TIME_FACE_H_
 
-#include "simple_clock_face.h"
-#include "world_clock_face.h"
-#include "preferences_face.h"
-#include "set_time_face.h"
-#include "pulsometer_face.h"
-#include "thermistor_readout_face.h"
-#include "thermistor_logging_face.h"
-#include "character_set_face.h"
-#include "beats_face.h"
-#include "day_one_face.h"
-#include "voltage_face.h"
-#include "stopwatch_face.h"
-#include "totp_face.h"
-#include "lis2dh_logging_face.h"
-#include "demo_face.h"
-#include "hello_there_face.h"
-#include "sunrise_sunset_face.h"
-#include "countdown_face.h"
-#include "blinky_face.h"
-#include "moon_phase_face.h"
-#include "mars_time_face.h"
-// New includes go above this line.
+#include "movement.h"
 
-#endif // MOVEMENT_FACES_H_
+typedef struct {
+    uint8_t unused;
+} mars_time_state_t;
+
+void mars_time_face_setup(movement_settings_t *settings, uint8_t watch_face_index, void ** context_ptr);
+void mars_time_face_activate(movement_settings_t *settings, void *context);
+bool mars_time_face_loop(movement_event_t event, movement_settings_t *settings, void *context);
+void mars_time_face_resign(movement_settings_t *settings, void *context);
+
+#define mars_time_face ((const watch_face_t){ \
+    mars_time_face_setup, \
+    mars_time_face_activate, \
+    mars_time_face_loop, \
+    mars_time_face_resign, \
+    NULL, \
+})
+
+#endif // MARS_TIME_FACE_H_
+
