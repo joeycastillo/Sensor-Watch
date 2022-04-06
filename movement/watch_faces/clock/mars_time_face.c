c4de155083d1f5574138976d7d697e572dc12a03
Joey Castillo <joeycastillo@utexas.edu>
2022-04-06 15:55:06 -0400

    mars clock: formatting

--- movement/watch_faces/clock/mars_time_face.c
+++ movement/watch_faces/clock/mars_time_face.c
@@ -94,13 +94,13 @@ static void _update(movement_settings_t *settings, mars_time_state_t *state) {
         if (sol < 1000) sprintf(&buf[0], "%s  Sol%3d", site_names[state->current_site], sol);
         else sprintf(&buf[0], "%s $%6d", site_names[state->current_site], sol);
         watch_clear_colon();
-        watch_clear_indicator(WATCH_INDICATOR_24H);        
+        watch_clear_indicator(WATCH_INDICATOR_24H);
     } else {
         mars_clock_hms_t mars_time;
         _h_to_hms(&mars_time, lmt);
         sprintf(&buf[0], "%s  %02d%02d%02d", site_names[state->current_site], mars_time.hour, mars_time.minute, mars_time.second);
         watch_set_colon();
-        watch_set_indicator(WATCH_INDICATOR_24H);        
+        watch_set_indicator(WATCH_INDICATOR_24H);
     }
 
     watch_display_string(buf, 0);
