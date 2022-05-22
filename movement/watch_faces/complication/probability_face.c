6ed757af2b7778e6944a5a8761672bc8d03620e2
Spencer Bywater <spencer.bywater@gmail.com>
2022-05-22 16:42:37 -0700

    Adjust probability watch face timeout/LE behavior (#72)
    Display SLEEP instead of timing out

--- movement/watch_faces/complication/probability_face.c
+++ movement/watch_faces/complication/probability_face.c
@@ -166,8 +166,8 @@ bool probability_face_loop(movement_event_t event, movement_settings_t *settings
             // Dice rolling animation begins on next tick and new roll will be displayed on completion
             movement_request_tick_frequency(PROBABILITY_ANIMATION_TICK_FREQUENCY);
             break;
-        case EVENT_TIMEOUT:
-            movement_move_to_face(0);
+        case EVENT_LOW_ENERGY_UPDATE:
+            watch_display_string("SLEEP ", 4);
             break;
         default:
             break;
