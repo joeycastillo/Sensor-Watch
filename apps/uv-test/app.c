ca6e7f61850987615fb3cda981222abb2ce9b03b
Wesley Ellis <tahnok@gmail.com>
2022-07-19 09:54:41 -0400

    WIP add i2c registers

--- apps/uv-test/app.c
+++ apps/uv-test/app.c
@@ -27,6 +27,38 @@ static void cb_tick(void) {
 #define SI1133_REG_PART_ID 0x00
 #define SI1133_REG_HW_ID 0x01
 #define SI1133_REG_REV_ID 0x02
+#define SI1133_REG_COMMAND 0x0B
+#define SI1133_REG_RESPONSE0 0x10
+#define SI1133_REG_RESPONSE1 0x11
+#define SI1133_REG_IRQ_STATUS 0x12
+#define SI1133_REG_HOSTOUT0 0x13
+#define SI1133_REG_HOSTOUT1 0x14
+#define SI1133_REG_HOSTOUT2 0x15
+#define SI1133_REG_HOSTOUT3 0x16
+#define SI1133_REG_HOSTOUT4 0x17
+#define SI1133_REG_HOSTOUT5 0x18
+#define SI1133_REG_HOSTOUT6 0x19
+#define SI1133_REG_HOSTOUT7 0x1a
+#define SI1133_REG_HOSTOUT8 0x1b
+#define SI1133_REG_HOSTOUT9 0x1c
+#define SI1133_REG_HOSTOUT10 0x1d
+#define SI1133_REG_HOSTOUT11 0x1e
+#define SI1133_REG_HOSTOUT12 0x1f
+#define SI1133_REG_HOSTOUT13 0x20
+#define SI1133_REG_HOSTOUT14 0x21
+#define SI1133_REG_HOSTOUT15 0x22
+#define SI1133_REG_HOSTOUT16 0x23
+#define SI1133_REG_HOSTOUT17 0x24
+#define SI1133_REG_HOSTOUT18 0x25
+#define SI1133_REG_HOSTOUT19 0x26
+#define SI1133_REG_HOSTOUT20 0x27
+#define SI1133_REG_HOSTOUT21 0x28
+#define SI1133_REG_HOSTOUT22 0x29
+#define SI1133_REG_HOSTOUT23 0x2a
+#define SI1133_REG_HOSTOUT24 0x2b
+#define SI1133_REG_HOSTOUT25 0x2c
+
+#define SI1133_CMD_RESET 0x01
 
 void app_init(void) {
     watch_enable_display();
@@ -68,8 +100,11 @@ bool app_loop(void) {
         printf("rev_id: %02x\r\n", rev_id);
 
         // do a reset by writing 0x0B 0x00 
+        watch_i2c_write8(SI1133_ADDR, SI1133_REG_COMMAND, SI1133_CMD_RESET);
         // this writes to register 0x0B the value 0x00 (which is a reset)
         //we should then be able to read RESPONSE0 (0x11) to check some of the bits
+        uint8_t response = watch_i2c_read8(SI1133_ADDR, SI1133_REG_RESPONSE0);
+        printf("response: %02x\r\n", response);
 
     }
     return true;
