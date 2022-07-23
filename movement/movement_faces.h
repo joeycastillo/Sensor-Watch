d9d5e6ac68bdc3fc88cab1acb4a8544f0459479e
Josh Berson <low.note0488@fastmail.com>
2022-07-23 14:00:39 +0200

    movement_play_alarm()

--- movement/movement_faces.h
+++ movement/movement_faces.h
@@ -54,7 +54,7 @@
 #include "tomato_face.h"
 #include "probability_face.h"
 #include "wake_face.h"
-#include "interval_face.h"
+// #include "interval_face.h"
 // New includes go above this line.
 
 #endif // MOVEMENT_FACES_H_
