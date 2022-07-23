655e0d998367c4e05291ed279bdf226f5404c326
Josh Berson <low.note0488@fastmail.com>
2022-07-23 13:45:46 +0200

    touching up wake_face per @joeycastillo’s notes

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -29,7 +29,10 @@
 
 const watch_face_t watch_faces[] = {
     simple_clock_face,
-    interval_face,
+    world_clock_face,
+    sunrise_sunset_face,
+    moon_phase_face,
+    thermistor_readout_face,
     preferences_face,
     set_time_face,
 };
