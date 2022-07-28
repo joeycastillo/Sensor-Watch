22dd4296f20603828d8e5c68b5ae558e461e9a85
Wesley Ellis <tahnok@gmail.com>
2022-07-28 09:47:59 -0300

    WIP manual loop control, add fatal_error

--- apps/uv-test/app.c
+++ apps/uv-test/app.c
@@ -1,9 +1,12 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
 #include "watch.h"
 #include "watch_extint.h"
+
+static void fatal_error(char *error_msg, uint8_t error_no, uint8_t extra);
 
 static void cb_light_pressed(void) {
 }
@@ -14,18 +17,16 @@ static void cb_mode_pressed(void) {
 static void cb_alarm_pressed(void) {
 }
 
+/*
 static bool si1133_ready = false;
 
 static void cb_si1133_int(void) {
     si1133_ready = true;
 }
+*/
 
-static bool tick = false;
+
 static char buf[13] = {0};
-
-static void cb_tick(void) {
-    tick = true;
-}
 
 #define SI1133_ADDR 0x55 //alt is 0x52
 
@@ -74,6 +75,9 @@ static void cb_tick(void) {
 #define SI1133_CMD_FORCE 0x11
 #define SI1133_CMD_PAUSE 0x12
 #define SI1133_CMD_START 0x13
+
+#define SI1133_CMD_STATUS_ERROR 0x10
+#define SI1133_CMD_STATUS_ERROR_MASK 0x0f
 
 #define SI1133_PARAM_QUERY_MASK 0x40 // 0b01......
 #define SI1133_PARAM_SET_MASK 0x80 // 0b10......
@@ -191,7 +195,7 @@ typedef enum {
 
 void error(char *msg);
 void si1133_init(void);
-void si1133_start_measurement(void);
+bool si1133_start_measurement(void);
 
 /**
  * @brief configure channel
@@ -219,16 +223,22 @@ int si1133_configure_channel(
 int si1133_set_param(si1133_params param, uint8_t value);
 
 void app_init(void) {
-    delay_ms(1000);
+}
 
-    watch_enable_display();
+void app_wake_from_backup(void) {
+    printf("waking from backup sleep\r\n");
+}
+
+void app_setup(void) {
+    watch_enable_leds();
 
     watch_enable_external_interrupts();
     watch_register_interrupt_callback(BTN_MODE, cb_mode_pressed, INTERRUPT_TRIGGER_RISING);
     watch_register_interrupt_callback(BTN_LIGHT, cb_light_pressed, INTERRUPT_TRIGGER_RISING);
     watch_register_interrupt_callback(BTN_ALARM, cb_alarm_pressed, INTERRUPT_TRIGGER_RISING);
 
-    watch_enable_leds();
+
+    watch_enable_display();
 
     si1133_init();
 
@@ -238,33 +248,41 @@ void app_init(void) {
      *  - callback fires immediately after registering it, but doesn't seem to fire again after that?
      *  - callback seems to wake device from sleep (but callback isn't triggered) and measurement isn't ready when this happens
      *  - pin does seem to go low before when I do poll the status register
-    */
+     *
+     *  ----
+     *
+     *  It's possible this was only firing once because it was in app_init, not setup, and I need to re-register the callback everytime?
     watch_register_extwake_callback(A2, cb_si1133_int, false); // trigger on falling edge
     watch_enable_pull_up(A2);
+    */
 
-    watch_rtc_register_periodic_callback(cb_tick, 1);
-}
-
-void app_wake_from_backup(void) {
-}
-
-void app_setup(void) {
 }
 
 void app_prepare_for_standby(void) {
+    printf("going to standby sleep\r\n");
 }
 
 void app_wake_from_standby(void) {
+    printf("waking from standby sleep\r\n");
 }
 
-char *error_msg = "";
-uint8_t error_count = 0;
+void fatal_error(char *error_msg, uint8_t error_no, uint8_t extra) {
+    watch_set_colon();
+    sprintf(buf, "    %02x%0x   ", error_no, extra);
+    watch_display_string(buf, 0);
+    uint8_t loops = 255;
+    while(loops--) {
+        printf("%s\r\n", error_msg);
 
-void error(char *msg) {
-    printf("error: %s\r\n", msg);
-    error_msg = msg;
-    watch_set_led_color(255, 0);
-    error_count++;
+        watch_set_indicator(WATCH_INDICATOR_BELL);
+        watch_set_led_yellow();
+
+        delay_ms(500);
+        watch_clear_all_indicators();
+        watch_set_led_color(0,0);
+        delay_ms(500);
+    }
+    watch_enter_sleep_mode();
 }
 
 void si1133_init(void) {
@@ -272,12 +290,12 @@ void si1133_init(void) {
 
     uint8_t part_id = watch_i2c_read8(SI1133_ADDR, SI1133_REG_PART_ID);
     if (part_id != 0x33) {
-        error("bad part id");
+        fatal_error("bad part id", 0, 0);
     }
     watch_i2c_write8(SI1133_ADDR, SI1133_REG_COMMAND, SI1133_CMD_RESET);
     uint8_t response = watch_i2c_read8(SI1133_ADDR, SI1133_REG_RESPONSE0);
-    if (response != 0) {
-        error("failed to reset");
+    if (response & SI1133_CMD_STATUS_ERROR) {
+        fatal_error("failed to reset", 1, response);
     }
 
     // from linux driver it looks like this is probably a mask for which channel should be enabled but it's not in datasheet
@@ -292,7 +310,7 @@ void si1133_init(void) {
             SI1133_DECIM_0,
             SI1133_RANGING_OFF,
             SI1133_HW_GAIN_11,
-            SI1133_SW_GAIN_0,
+            SI1133_SW_GAIN_4,
             SI1133_16_BIT,
             SI1133_POST_SHIFT_0,
             SI1133_THRESH_DISABLE,
@@ -356,44 +374,51 @@ int si1133_configure_channel(
 }
 
 uint8_t readings = 0;
+uint8_t error_count = 0;
 
-void si1133_start_measurement(void) {
+bool si1133_start_measurement(void) {
     watch_i2c_write8(SI1133_ADDR, SI1133_REG_COMMAND, SI1133_CMD_FORCE);
     uint8_t response = watch_i2c_read8(SI1133_ADDR, SI1133_REG_RESPONSE0);
-    if (response & 0x10) {
-        error("bad response code");
-        printf("error! %02x\r\n", (response & 0x08));
+    if (response & SI1133_CMD_STATUS_ERROR) {
+        error_count++;
+        printf("error! bad respones from start measurement: %02x\r\n", (response & SI1133_CMD_STATUS_ERROR_MASK));
+        return false;
     }
+    return true;
 }
 
 bool app_loop(void) {
-    if (tick) {
-        printf("--- tick\r\n");
-        tick = false;
+    printf("--- tick\r\n");
 
-        if (strlen(error_msg) > 0) {
-            printf("error: %s\r\n", error_msg);
-            watch_set_led_color(0, 255);
-        }
-
-        si1133_start_measurement();
-
-        uint8_t irq_status = watch_i2c_read8(SI1133_ADDR, SI1133_REG_IRQ_STATUS);
-        while(irq_status != 0x01) {
-            delay_ms(10);
-            irq_status = watch_i2c_read8(SI1133_ADDR, SI1133_REG_IRQ_STATUS);
-        }
-
-        uint8_t hostout0 = watch_i2c_read8(SI1133_ADDR, SI1133_REG_HOSTOUT0);
-        uint8_t hostout1 = watch_i2c_read8(SI1133_ADDR, SI1133_REG_HOSTOUT1);
-        uint16_t adccount = hostout0 << 8 | hostout1;
-        sprintf(buf, "%02x%02x%04x",error_count, readings, adccount);
-        watch_display_string(buf, 0);
-        printf("reading: %04x\r\n", adccount);
-        printf("reading count: %02x\r\n", readings);
-        printf("error count: %02x\r\n", error_count);
-        readings++;
-
+    if(!si1133_start_measurement()) {
+        goto end;
     }
+
+    uint32_t ticks = 0;
+    uint8_t irq_status = watch_i2c_read8(SI1133_ADDR, SI1133_REG_IRQ_STATUS);
+    while(irq_status != 0x01) {
+        delay_ms(10);
+        ticks++;
+        if (ticks > 1000) {
+            error_count++;
+            printf("timeout waiting for reading!!\r\n");
+            break;
+        }
+        irq_status = watch_i2c_read8(SI1133_ADDR, SI1133_REG_IRQ_STATUS);
+    }
+    printf("ticks %ud\r\n", ticks);
+
+    uint8_t hostout0 = watch_i2c_read8(SI1133_ADDR, SI1133_REG_HOSTOUT0);
+    uint8_t hostout1 = watch_i2c_read8(SI1133_ADDR, SI1133_REG_HOSTOUT1);
+    uint16_t adccount = hostout0 << 8 | hostout1;
+    sprintf(buf, "%02x%02x%04x",error_count, readings, adccount);
+    watch_display_string(buf, 0);
+    printf("reading: %04x\r\n", adccount);
+    printf("reading count: %02x\r\n", readings);
+    printf("error count: %02x\r\n", error_count);
+    readings++;
+
+end:
+    delay_ms(1000);
     return true;
 }
