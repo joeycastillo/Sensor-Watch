e6784339e01ef38b0bc0c612670083b4f34a0218
Joey Castillo <joeycastillo@utexas.edu>
2022-09-16 22:54:36 -0500

    add script for bulk flashing boards

---
+++ utils/movement_bulk_installer/shipboards.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+RED='\033[0;101m'
+GREEN='\033[1;32m'
+END='\033[0m'
+while [ true ] ; do
+    echo "Waiting for board"
+    false
+    while [ true ] ; do
+        stat /Volumes/WATCHBOOT &> /dev/null
+        if [ $? -eq 0 ]
+        then
+            break
+        fi
+        sleep 0.5
+    done
+    sleep 0.5
+    clear
+    cp ./standard-green.uf2 /Volumes/WATCHBOOT/
+    if [ $? -eq 0 ]
+    then
+        echo -e "${GREEN}Success!${END} Board flashed successfully!"
+    else
+        echo -e "${RED}FAILURE:${END} Board did not flash successfully."
+        echo -ne '\a'
+        echo -ne '\a'
+    fi
+    while [ true ] ; do
+        stat /Volumes/WATCHBOOT &> /dev/null
+        if [ $? -eq 0 ]
+        then
+            sleep 0.5
+        else
+            break
+        fi
+    done
+    echo -ne '\a'
+done
