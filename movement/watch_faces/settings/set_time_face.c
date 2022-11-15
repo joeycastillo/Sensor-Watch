9acf827206c779c7a196a69f7e4fefdfd46df3ea
TheOnePerson <a.nebinger@web.de>
2022-11-15 08:08:13 +0100

    set time face: add quick cycling through hours, minutes, etc. and handle February 29th properly

--- movement/watch_faces/settings/set_time_face.c
+++ movement/watch_faces/settings/set_time_face.c
@@ -29,32 +29,12 @@
 #define SET_TIME_FACE_NUM_SETTINGS (7)
 const char set_time_face_titles[SET_TIME_FACE_NUM_SETTINGS][3] = {"HR", "M1", "SE", "YR", "MO", "DA", "ZO"};
 
-void set_time_face_setup(movement_settings_t *settings, uint8_t watch_face_index, void ** context_ptr) {
-    (void) settings;
-    (void) watch_face_index;
-    if (*context_ptr == NULL) *context_ptr = malloc(sizeof(uint8_t));
-}
+static bool _quick_ticks_running;
 
-void set_time_face_activate(movement_settings_t *settings, void *context) {
-    (void) settings;
-    *((uint8_t *)context) = 0;
-    movement_request_tick_frequency(4);
-}
-
-bool set_time_face_loop(movement_event_t event, movement_settings_t *settings, void *context) {
-    uint8_t current_page = *((uint8_t *)context);
+static void _handle_alarm_button(movement_settings_t *settings, watch_date_time date_time, uint8_t current_page) {
+    // handles short of long pressing of the alarm button
     const uint8_t days_in_month[12] = {31, 28, 31, 30, 31, 30, 30, 31, 30, 31, 30, 31};
-    watch_date_time date_time = watch_rtc_get_date_time();
 
-    switch (event.event_type) {
-        case EVENT_MODE_BUTTON_UP:
-            movement_move_to_next_face();
-            return false;
-        case EVENT_LIGHT_BUTTON_UP:
-            current_page = (current_page + 1) % SET_TIME_FACE_NUM_SETTINGS;
-            *((uint8_t *)context) = current_page;
-            break;
-        case EVENT_ALARM_BUTTON_UP:
             switch (current_page) {
                 case 0: // hour
                     date_time.unit.hour = (date_time.unit.hour + 1) % 24;
@@ -72,22 +52,76 @@ bool set_time_face_loop(movement_event_t event, movement_settings_t *settings, v
                 case 4: // month
                     date_time.unit.month = (date_time.unit.month % 12) + 1;
                     break;
-                case 5: // day
-                    date_time.unit.day = date_time.unit.day + 1;
-                    // can't set to the 29th on a leap year. if it's february 29, set to 11:59 on the 28th.
-                    // and it should roll over.
-                    if (date_time.unit.day > days_in_month[date_time.unit.month - 1]) {
-                        date_time.unit.day = 1;
+        case 5: { // day
+            uint32_t tmp_day = date_time.unit.day;   // use a temporary variable to avoid messing up the months
+            tmp_day = tmp_day + 1;
+            // handle February 29th on a leap year
+            if (((tmp_day > days_in_month[date_time.unit.month - 1]) && (date_time.unit.month != 2 || (date_time.unit.year % 4) != 0))
+                || (date_time.unit.month == 2 && (date_time.unit.year % 4) == 0 && tmp_day > 29)) {
+                tmp_day = 1;
                     }
+            date_time.unit.day = tmp_day;
                     break;
+        }
                 case 6: // time zone
                     settings->bit.time_zone++;
                     if (settings->bit.time_zone > 40) settings->bit.time_zone = 0;
                     break;
             }
             watch_rtc_set_date_time(date_time);
+}
+
+static void _abort_quick_ticks() {
+    if (_quick_ticks_running) {
+        _quick_ticks_running = false;
+        movement_request_tick_frequency(4);
+    }
+}
+
+void set_time_face_setup(movement_settings_t *settings, uint8_t watch_face_index, void ** context_ptr) {
+    (void) settings;
+    (void) watch_face_index;
+    if (*context_ptr == NULL) *context_ptr = malloc(sizeof(uint8_t));
+}
+
+void set_time_face_activate(movement_settings_t *settings, void *context) {
+    (void) settings;
+    *((uint8_t *)context) = 0;
+    movement_request_tick_frequency(4);
+    _quick_ticks_running = false;
+}
+
+bool set_time_face_loop(movement_event_t event, movement_settings_t *settings, void *context) {
+    uint8_t current_page = *((uint8_t *)context);
+    watch_date_time date_time = watch_rtc_get_date_time();
+
+    switch (event.event_type) {
+        case EVENT_TICK:
+            if (_quick_ticks_running) _handle_alarm_button(settings, date_time, current_page);
+            break;
+        case EVENT_ALARM_LONG_PRESS:
+            if (current_page != 2) {
+                _quick_ticks_running = true;
+                movement_request_tick_frequency(8);
+            }
+            break;
+        case EVENT_ALARM_LONG_UP:
+            _abort_quick_ticks();
+            break;
+        case EVENT_MODE_BUTTON_UP:
+            _abort_quick_ticks();
+            movement_move_to_next_face();
+            return false;
+        case EVENT_LIGHT_BUTTON_UP:
+            current_page = (current_page + 1) % SET_TIME_FACE_NUM_SETTINGS;
+            *((uint8_t *)context) = current_page;
+            break;
+        case EVENT_ALARM_BUTTON_UP:
+            _abort_quick_ticks();
+            _handle_alarm_button(settings, date_time, current_page);
             break;
         case EVENT_TIMEOUT:
+            _abort_quick_ticks();
             movement_move_to_face(0);
             break;
         default:
@@ -121,7 +155,7 @@ bool set_time_face_loop(movement_event_t event, movement_settings_t *settings, v
     }
 
     // blink up the parameter we're setting
-    if (event.subsecond % 2) {
+    if (event.subsecond % 2 && !_quick_ticks_running) {
         switch (current_page) {
             case 0:
             case 3:
