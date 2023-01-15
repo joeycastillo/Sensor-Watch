e35c40e29d2e588e2707b260789c7da2746480f1
Konrad Rieck <konrad@mlsec.org>
2023-01-15 19:29:15 +0100

    support for beeps similar to stop watch

--- movement/watch_faces/complication/countdown_face.c
+++ movement/watch_faces/complication/countdown_face.c
@@ -77,6 +77,12 @@ static inline void load_countdown(countdown_state_t *state) {
     state->seconds = state->set_seconds;
 }
 
+static inline void button_beep(movement_settings_t *settings) {
+    // play a beep as confirmation for a button press (if applicable)
+    if (settings->bit.button_should_sound)
+        watch_buzzer_play_note(BUZZER_NOTE_C7, 50);
+}
+
 static void start(countdown_state_t *state, movement_settings_t *settings) {
     watch_date_time now = watch_rtc_get_date_time();
 
@@ -226,10 +232,12 @@ bool countdown_face_loop(movement_event_t event, movement_settings_t *settings, 
                     break;
                 case cd_paused:
                     reset(state);
+                    button_beep(settings);
                     break;
                 case cd_reset:
                     state->mode = cd_setting;
                     movement_request_tick_frequency(4);
+                    button_beep(settings);
                     break;
                 case cd_setting:
                     state->selection++;
@@ -247,12 +255,14 @@ bool countdown_face_loop(movement_event_t event, movement_settings_t *settings, 
             switch(state->mode) {
                 case cd_running:
                     pause(state);
+                    button_beep(settings);
                     break;
                 case cd_reset:
                 case cd_paused:
                     if (!(state->hours == 0 && state->minutes == 0 && state->seconds == 0)) {
                         // Only start the timer if we have a valid time.
                         start(state, settings);
+                        button_beep(settings);
                     }
                     break;
                 case cd_setting:
