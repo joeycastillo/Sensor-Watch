61b8cb3d881d3288b0a1ece8404506f89d42ca36
Þorsteinn Jón Gautason <thg191@protonmail.com>
2023-02-20 11:36:55 +0000

    fixed for loops that didn't cover whole array, long mode press snaps back to default face

--- movement/watch_faces/complication/discgolf_face.c
+++ movement/watch_faces/complication/discgolf_face.c
@@ -1,36 +1,45 @@
-/*
- * MIT License
- *
- * Copyright (c) 2023 Þorsteinn Jón Gautason
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-/////////////////////////////////////////////////////////////////////////////////////
-
 #include <stdlib.h>
 #include <string.h>
-#include "discgolf_face.h"                                      // Remember to change number of courses in this file
-#include "watch.h"
+#include "discgolf_face.h"
+#include "watch.h"                                              // Remember to change number of courses in this file
 #include "watch_utility.h"
 
+/* 
+ * Keep track of scores in discgolf or golf!
+ * The watch face operates in three different modes:
+ *
+ *  - dg_setting: Select a course
+ *      Enter this mode by holding down the light button. The screen will display
+ *      the label for the hole and the lowest score since last boot. 
+ *      Press alarm to loop through the holes. Press the light button to make a 
+ *      selection. This will reset all scores and start a new game in dg_idle mode.
+ *
+ *  -dg_idle: We're playing a hole
+ *      This either shows your current score relative to par, or the score for a
+ *      particular hole. 
+ *      At the start of a game, press alarm to loop through the holes and leave it
+ *      your starting hole. For optimal experience, play the course linearly after that
+ *      If you're viewing the hole you're supposed to be playing, the watch face will
+ *      display your score relative to par.
+ *      Use the alarm button to view other holes than the one you're playing, in which
+ *      case the input score for that hole will be displayed, in case it needs changing.
+ *      Long press the alarm button to snap back to currently playing hole.
+ *      To input scores for a hole in this mode, press the light button.
+ *
+ *  -dg_scoring: Input score for a hole
+ *      In this mode, if the score is 0 (hasn't been entered during this round),
+ *      it will blink, indicating we're in scoring mode. Press the alarm button
+ *      to increment the score up until 15, in which case it loops back to 0. 
+ *      Press the light button to save the score for that hole, advance one hole
+ *      if you're not editing an already input score, and returning to idle mode.
+ *
+ *  When all scores have been entered, the LAP indicator turns on. At that point, if we enter 
+ *  dg_setting to select a course, the score for that round is evaluated against the current 
+ *  lowest score for that course, and saved if it is better.
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////
 // Enter course data
+/* Initialize lowest scores with a high number */
 int8_t best[courses];
 
 static const uint8_t pars[][18] = {
@@ -84,7 +93,7 @@ static inline void beep(movement_settings_t *settings) {
 
 /* Prep for a new round */
 static inline void reset(discgolf_state_t *state) {
-    for (int i = 0; i < holes[state->course] - 1; i++) {
+    for (int i = 0; i < holes[state->course]; i++) {
         state->scores[i] = 0;
     }
     state->hole = 1;
@@ -95,7 +104,7 @@ static inline void reset(discgolf_state_t *state) {
 /* Total number of throws so far */
 static inline uint8_t score_sum(discgolf_state_t *state) {
     uint8_t sum = 0;
-    for (int i = 0; i < holes[state->course] - 1; i++) {
+    for (int i = 0; i < holes[state->course]; i++) {
         sum = sum + state->scores[i];
     }
     return sum;
@@ -116,7 +125,7 @@ static inline int8_t calculate_score(discgolf_state_t *state) {
     uint8_t par_sum = 0;
     uint8_t score_sum = 0;
     
-    for (int i = 0; i < holes[state->course] - 1; i++) {
+    for (int i = 0; i < holes[state->course]; i++) {
         if (state->scores[i] > 0) {
             par_sum = par_sum + pars[state->course][i];
             score_sum = score_sum + state->scores[i];
@@ -136,6 +145,7 @@ static inline void store_best(discgolf_state_t *state) {
     }
 }
 
+/* Configuration at boot, the high score array can be initialized with your high scores if they're known */
 void discgolf_face_setup(movement_settings_t *settings, uint8_t watch_face_index, void ** context_ptr) {
     (void) settings;
     (void) watch_face_index;
@@ -189,6 +199,12 @@ bool discgolf_face_loop(movement_event_t event, movement_settings_t *settings, v
         case EVENT_MODE_BUTTON_UP:
             if ( state->mode != dg_scoring ) {
 				movement_move_to_next_face();
+            }
+            break;
+        /* Go to default face if not scoring */
+        case EVENT_MODE_LONG_PRESS:
+            if ( state->mode != dg_scoring ) {
+                movement_move_to_face(0);
             }
             break;
         case EVENT_LIGHT_BUTTON_UP:
