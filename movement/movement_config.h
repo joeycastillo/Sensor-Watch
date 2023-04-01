b432730d762bc74778d624ba4f8379fd1787c545
randogoth <kflux@posteo.de>
2023-04-01 10:07:20 +0300

    reset config

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -29,8 +29,10 @@
 
 const watch_face_t watch_faces[] = {
     simple_clock_face,
-    place_face,
-    places_face,
+    world_clock_face,
+    sunrise_sunset_face,
+    moon_phase_face,
+    stopwatch_face,
     preferences_face,
     set_time_face,
 };
