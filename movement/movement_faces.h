eb7168177957acae7cffc962e83f88893f867f23
joeycastillo <joeycastillo@utexas.edu>
2023-04-16 10:46:52 -0400

    Revert "Coin & Dice Toss & Geomantic Divination Watch Faces (#235)"
    This reverts commit 34030bf3e403b2043dcd9f960189d93190dcbb0c.

--- movement/movement_faces.h
+++ movement/movement_faces.h
@@ -87,8 +87,6 @@
 #include "invaders_face.h"
 #include "world_clock2_face.h"
 #include "time_left_face.h"
-#include "toss_up_face.h"
-#include "geomancy_face.h"
 #include "dual_timer_face.h"
 // New includes go above this line.
 
