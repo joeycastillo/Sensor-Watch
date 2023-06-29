b49259e4e0172c5025c95ea12fda13f9f71ebb0b
joeycastillo <joeycastillo@utexas.edu>
2023-06-29 16:17:19 -0400

    add basic flashlight function

--- movement/movement_faces.h
+++ movement/movement_faces.h
@@ -92,6 +92,7 @@
 #include "geomancy_face.h"
 #include "dual_timer_face.h"
 #include "simple_clock_bin_led_face.h"
+#include "flashlight_face.h"
 // New includes go above this line.
 
 #endif // MOVEMENT_FACES_H_
