3ee32c6e57e9a741355251f33fbcc323ded249d8
Wesley Aptekar-Cassels <me@wesleyac.com>
2023-11-13 00:16:13 -0500

    Use legacy buzzer functions when playing default tune.
    This allows the default tune to be played in LE mode.
    
    Fixes: #275

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -49,7 +49,8 @@ const watch_face_t watch_faces[] = {
  */
 #define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 2) // or (0)
 
-/* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options */
+/* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options.
+ * Custom tunes do not currently work in LE mode. */
 #define SIGNAL_TUNE_DEFAULT
 
 #endif // MOVEMENT_CONFIG_H_
