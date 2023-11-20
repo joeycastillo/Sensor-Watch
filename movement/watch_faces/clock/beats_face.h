aba6ba0d4bd77a9d9bd6b9efafa619e433096480
Alex Utter <ooterness@gmail.com>
2023-11-19 19:05:40 -0800

    Fix listed author for "beats_face".

--- movement/watch_faces/clock/beats_face.h
+++ movement/watch_faces/clock/beats_face.h
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 Curtis J. Brown <mrbrown8@juno.com>
+ * Copyright (c) 2023 Wesley Ellis <https://github.com/tahnok>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
