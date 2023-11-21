3254ce40dfd6842b8fa6a07aeee148069c58c55b
Alex Utter <ooterness@gmail.com>
2023-11-20 20:56:07 -0800

    Designate @tahnok as author per request from @wryun

--- movement/watch_faces/complication/totp_face_lfs.c
+++ movement/watch_faces/complication/totp_face_lfs.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2022 James Haggerty (https://github.com/wryun)
+ * Copyright (c) 2022 Wesley Ellis (https://github.com/tahnok)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
