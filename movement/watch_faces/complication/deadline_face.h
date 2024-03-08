509f49743a84dd6e9962621614478e861bb1d976
Konrad Rieck <konrad@mlsec.org>
2024-03-08 14:50:32 +0100

    Fixed bugs and improved watchface
    - A background task is only scheduled if the alarm option is activated. If the option is enabled, an alarm sounds when the next deadline is reached. If the option is disabled, no alarm sounds and the watch can enter low energy sleep.
    
    - Fixed tick frequency error. During running mode, the clock ticks at 1Hz. This is set in the init function `_running_init` and thus ensures that we do not run too fast when returning from the setting mode.
    
    - Minor corrections to comments and indentations.

--- movement/watch_faces/complication/deadline_face.h
+++ movement/watch_faces/complication/deadline_face.h
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 Konrad Rieck
+ * Copyright (c) 2023-2024 Konrad Rieck
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
