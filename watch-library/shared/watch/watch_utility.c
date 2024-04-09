7a38d68268e65b4fdb653fc0beadf87194bb8644
mrbrown8 <mrbrown8@juno.com>
2024-04-09 16:20:47 -0600

    Update watch_utility.c

--- watch-library/shared/watch/watch_utility.c
+++ watch-library/shared/watch/watch_utility.c
@@ -27,15 +27,15 @@
 #include "movement.h"
 
 const char * watch_utility_get_weekday(watch_date_time date_time) {
-    movement_settings_t movement_settings = (movement_settings_t) watch_get_backup_data(0);
+    //movement_settings_t movement_settings = (movement_settings_t) watch_get_backup_data(0);
     uint8_t weekday_index;
 
     //static const char weekdays[7][3] = {"MO", "TU", "WE", "TH", "FR", "SA", "SU"};
-    static char weekdays_dict[4][7][3] = { {"MO", "TU", "WE", "TH", "FR", "SA", "SU"} ,   // EN
-                                           {"MO", "DI", "MI", "DO", "FR", "SA", "SO"} ,   // DE
-                                           {"LU", "ME", "MI", "JU", "VI", "SA", "DO"} ,   // ES
-                                           {"LU", "MA", "ME", "JE", "VE", "SA", "DI"} };  // FR
-    char (*p_dict_row)[7][3] = weekdays_dict;  // point to the first row; default is English, esp. if there's no config face
+    static const char weekdays_dict[4][7][3] = { {"MO", "TU", "WE", "TH", "FR", "SA", "SU"} ,   // EN
+                                                 {"MO", "DI", "MI", "DO", "FR", "SA", "SO"} ,   // DE
+                                                 {"LU", "ME", "MI", "JU", "VI", "SA", "DO"} ,   // ES
+                                                 {"LU", "MA", "ME", "JE", "VE", "SA", "DI"} };  // FR
+    const char (*p_dict_row)[7][3] = weekdays_dict;  // point to the first row; default is English, esp. if there's no config face
 
     weekday_index = watch_utility_get_iso8601_weekday_number(date_time.unit.year + WATCH_RTC_REFERENCE_YEAR, date_time.unit.month, date_time.unit.day) - 1 ;
 
