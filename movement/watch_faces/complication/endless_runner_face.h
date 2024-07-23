503fcd6ebcbc9b215c7fe87a10594b92e9e61387
David Volovskiy <devolov@gmail.com>
2024-07-23 17:52:50 -0400

    Added author in header

--- movement/watch_faces/complication/endless_runner_face.h
+++ movement/watch_faces/complication/endless_runner_face.h
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2024 <#author_name#>
+ * Copyright (c) 2024 <David Volovskiy>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
