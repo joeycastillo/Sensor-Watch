2cdfa2d3b3bd59f17c20540b3326dd869dc90c10
David Volovskiy <devolov@gmail.com>
2024-08-17 02:45:22 -0400

    Set the debounce tick variables to 0 to make the face work the same as stock.

--- movement/movement.c
+++ movement/movement.c
@@ -23,8 +23,8 @@
  */
 
 #define MOVEMENT_LONG_PRESS_TICKS 64
-#define DEBOUNCE_TICKS_DOWN 2  // In terms of *7.8125ms
-#define DEBOUNCE_TICKS_UP   2  // In terms of *7.8125ms
+#define DEBOUNCE_TICKS_DOWN 0  // In terms of *7.8125ms
+#define DEBOUNCE_TICKS_UP   0  // In terms of *7.8125ms
 
 #include <stdio.h>
 #include <string.h>
