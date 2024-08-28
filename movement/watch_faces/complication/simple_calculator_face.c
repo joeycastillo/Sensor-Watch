7b2306156d8cccff489b204eacea469a118d4505
mcguirepr89 <mcguirepr89@gmail.com>
2024-08-28 12:12:42 -0400

    adding new simple_calculator_face for testing

---
+++ movement/watch_faces/complication/simple_calculator_face.c
@@ -0,0 +1,333 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Patrick McGuire
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "simple_calculator_face.h"
+
+void simple_calculator_face_setup(movement_settings_t *settings, uint8_t watch_face_index, void ** context_ptr) {
+    (void) settings;
+    (void) watch_face_index;
+    if (*context_ptr == NULL) {
+        *context_ptr = malloc(sizeof(simple_calculator_state_t));
+        memset(*context_ptr, 0, sizeof(simple_calculator_state_t));
+    }
+}
+
+void simple_calculator_face_activate(movement_settings_t *settings, void *context) {
+    (void) settings;
+    simple_calculator_state_t *state = (simple_calculator_state_t *)context;
+    state->placeholder = PLACEHOLDER_ONES;
+    movement_request_tick_frequency(4);
+}
+
+static void increment_placeholder(calculator_number_t *number, calculator_placeholder_t placeholder) {
+    switch (placeholder) {
+        case PLACEHOLDER_HUNDREDTHS:
+            number->hundredths = (number->hundredths + 1) % 10;
+            break;
+        case PLACEHOLDER_TENTHS:
+            number->tenths = (number->tenths + 1) % 10;
+            break;
+        case PLACEHOLDER_ONES:
+            number->ones = (number->ones + 1) % 10;
+            break;
+        case PLACEHOLDER_TENS:
+            number->tens = (number->tens + 1) % 10;
+            break;
+        case PLACEHOLDER_HUNDREDS:
+            number->hundreds = (number->hundreds + 1) % 10;
+            break;
+        case PLACEHOLDER_THOUSANDS:
+            number->thousands = (number->thousands + 1) % 10;
+            break;
+        default:
+            break;
+    }
+}
+
+static float convert_to_float(calculator_number_t number) {
+    float result = 0.0;
+
+    // Add the whole number portion
+    result += number.thousands * 1000.0f;
+    result += number.hundreds * 100.0f;
+    result += number.tens * 10.0f;
+    result += number.ones * 1.0f;
+
+    // Add the fractional portion
+    result += number.tenths * 0.1f;
+    result += number.hundredths * 0.01f;
+
+    // Round to nearest hundredth
+    result = roundf(result * 100) / 100;
+    return result;
+}
+
+static char* update_display_number(calculator_number_t *number, char *display_string) {
+    // Update the display string based on the new values
+    sprintf(display_string, "%d%d%d%d%d%d",
+            number->thousands,
+            number->hundreds,
+            number->tens,
+            number->ones,
+            number->tenths,
+            number->hundredths);
+
+    return display_string;
+}
+
+static void update_display_operation(simple_calculator_state_t *state) {
+    switch (state->operation) {
+        case 0:
+            watch_display_string("   Add", 4);
+            break;
+        case 1:
+            watch_display_string("   sub", 4);
+            break;
+        case 2:
+            watch_display_string("  n&ul", 4);
+            break;
+        case 3:
+            watch_display_string("   div", 4);
+            break;
+        case 4:
+            watch_display_string("  root", 4);
+            break;
+        case 5:
+            watch_display_string("   pow", 4);
+            break;
+    }
+}
+
+static void cycle_operation(simple_calculator_state_t *state) {
+    state->operation = (state->operation + 1) % 6; // Assuming there are 6 operations
+    printf("Current operation: %d\n", state->operation); // For debugging
+}
+
+
+static calculator_number_t convert_to_string(float number) {
+    calculator_number_t result;
+    int int_part = (int)number;
+    float decimal_part_float = ((number - int_part) * 100); // two decimal places
+    printf("decimal_part_float = %f\n", decimal_part_float); //For debugging
+    int decimal_part = round(decimal_part_float);
+    printf("decimal_part = %d\n", decimal_part); //For debugging
+
+    result.thousands = int_part / 1000 % 10;
+    result.hundreds = int_part / 100 % 10;
+    result.tens = int_part / 10 % 10;
+    result.ones = int_part % 10;
+
+    result.tenths = decimal_part / 10 % 10;
+    result.hundredths = decimal_part % 10;
+
+    return result;
+}
+
+bool simple_calculator_face_loop(movement_event_t event, movement_settings_t *settings, void *context) {
+    simple_calculator_state_t *state = (simple_calculator_state_t *)context;
+    int display_index;
+    float first_num_float, second_num_float, result_float; // For arithmetic operations
+    char display_string[7];
+    char temp_display_string[7];  // Temporary buffer for blinking effect
+
+    switch (event.event_type) {
+        case EVENT_ACTIVATE:
+            break;
+
+        case EVENT_TICK: 
+            // Blink the placeholder that is selected
+            switch (state->mode) {
+                case MODE_ENTERING_FIRST_NUM:
+                    // Update display string with current number
+                    update_display_number(&state->first_num, display_string);
+
+                    // Copy the updated display string to a temporary buffer
+                    strcpy(temp_display_string, display_string);
+
+                    // Determine the display index based on the placeholder
+                    display_index = 5 - state->placeholder;
+
+                    // Check if `event.subsecond` is even
+                    if (event.subsecond % 2 == 0) {
+                        // Replace the character at the index corresponding to the current placeholder with a space
+                        temp_display_string[display_index] = ' ';
+                    }
+
+                    // Display the (possibly modified) string
+                    watch_display_string(temp_display_string, 4);
+                    break;                
+                case MODE_CHOOSING:
+                    update_display_operation(state);
+                    break;
+                case MODE_ENTERING_SECOND_NUM:
+                    // Update display string with current number
+                    update_display_number(&state->second_num, display_string);
+
+                    // Copy the updated display string to a temporary buffer
+                    strcpy(temp_display_string, display_string);
+
+                    // Determine the display index based on the placeholder
+                    display_index = 5 - state->placeholder;
+
+                    // Check if `event.subsecond` is even
+                    if (event.subsecond % 2 == 0) {
+                        // Replace the character at the index corresponding to the current placeholder with a space
+                        temp_display_string[display_index] = ' ';
+                    }
+
+                    // Display the (possibly modified) string
+                    watch_display_string(temp_display_string, 4);
+                    break;
+                case MODE_VIEW_RESULTS:
+                    // Convert the numbers to float
+                    //state->first_num.thousands = 0; // For debugging
+                    //state->first_num.hundreds = 6; // For debugging
+                    //state->first_num.tens = 3; // For debugging
+                    //state->first_num.ones = 3; // For debugging
+                    //state->first_num.tenths = 0; // For debugging
+                    //state->first_num.hundredths = 2; // For debugging
+                    first_num_float = convert_to_float(state->first_num);
+                    printf("first_num_float = %f\n", first_num_float); // For debugging // For debugging
+                    //state->second_num.thousands = 0; // For debugging
+                    //state->second_num.hundreds = 7; // For debugging
+                    //state->second_num.tens = 5; // For debugging
+                    //state->second_num.ones = 3; // For debugging
+                    //state->second_num.tenths = 6; // For debugging
+                    //state->second_num.hundredths = 1; // For debugging
+                    second_num_float = convert_to_float(state->second_num);
+                    printf("second_num_float = %f\n", second_num_float); // For debugging
+
+                    // Perform the calculation based on the selected operation
+                    switch (state->operation) {
+                        case OP_ADD:
+                            result_float = first_num_float + second_num_float;
+                            break;
+                        case OP_SUB:
+                            result_float = first_num_float - second_num_float;
+                            break;
+                        case OP_MULT:
+                            result_float = first_num_float * second_num_float;
+                            break;
+                        case OP_DIV:
+                            if (second_num_float != 0) {
+                                result_float = first_num_float / second_num_float;
+                            } else {
+                                result_float = 0; // Handle division by zero
+                            }
+                            break;
+                        case OP_ROOT:
+                            result_float = sqrtf(first_num_float);
+                            break;
+                        case OP_POWER:
+                            result_float = powf(first_num_float, second_num_float); // Power operation
+                            break;
+                        default:
+                            result_float = 0; // Default case
+                            break;
+                    }
+
+                    result_float = roundf(result_float * 100.0f) / 100.0f;
+                    printf("result as float = %f\n", result_float); // For debugging
+
+                    // Convert the float result to a string
+                    state->result = convert_to_string(result_float);
+                    sprintf(display_string, "%d%d%d%d%d%d", // For debugging
+                        state->result.thousands, // For debugging
+                        state->result.hundreds, // For debugging
+                        state->result.tens, // For debugging
+                        state->result.ones, // For debugging
+                        state->result.tenths, // For debugging
+                        state->result.hundredths); // For debugging
+                    printf("result as string: %s\n", display_string); // For debugging
+
+                    // Update the display with the result
+                    update_display_number(&state->result, display_string);
+                    watch_display_string(display_string, 4);
+                    break;
+            }
+            break;
+        case EVENT_LIGHT_BUTTON_DOWN:
+            break;
+
+        case EVENT_LIGHT_BUTTON_UP:
+            switch (state->mode) {
+                case MODE_ENTERING_FIRST_NUM:
+                case MODE_ENTERING_SECOND_NUM:
+                    // Move to the next placeholder when the light button is pressed
+                    state->placeholder = (state->placeholder + 1) % 6; // Loop back to the start after PLACEHOLDER_THOUSANDS
+                    break;
+                case MODE_CHOOSING:
+                    cycle_operation(state);
+                    break;
+                case MODE_VIEW_RESULTS:
+                    break;
+            }
+            break;
+
+        case EVENT_ALARM_BUTTON_UP:
+            switch (state->mode) {
+                case MODE_ENTERING_FIRST_NUM:
+                    // Increment the digit in the current placeholder
+                    increment_placeholder(&state->first_num, state->placeholder);
+                    update_display_number(&state->first_num, display_string);
+                    break;
+                case MODE_CHOOSING:
+                    // Confirm and select the current operation
+                    printf("Selected operation: %d\n", state->operation); // For debugging
+                    state->mode = MODE_ENTERING_SECOND_NUM;                
+                    break;
+                case MODE_ENTERING_SECOND_NUM:
+                    // Increment the digit in the current placeholder
+                    increment_placeholder(&state->second_num, state->placeholder);
+                    update_display_number(&state->second_num, display_string);
+                    break;
+                case MODE_VIEW_RESULTS:
+                    break;
+            }
+            break;
+
+        case EVENT_MODE_BUTTON_DOWN:
+            state->mode = (state->mode + 1) % 4;
+            printf("Current mode: %d\n", state->mode); // For debugging
+            break;
+        case EVENT_TIMEOUT:
+            // Handle timeout (e.g., resign face)
+            break;
+
+        default:
+            return movement_default_loop_handler(event, settings);
+    }
+
+    return true;
+}
+
+void simple_calculator_face_resign(movement_settings_t *settings, void *context) {
+    (void) settings;
+    (void) context;
+    movement_request_tick_frequency(1);
+}
+
---
+++ movement/watch_faces/complication/simple_calculator_face.h
@@ -0,0 +1,94 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Patrick McGuire
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SIMPLE_CALCULATOR_FACE_H_
+#define SIMPLE_CALCULATOR_FACE_H_
+
+#include "movement.h"
+
+/*
+ * A DESCRIPTION OF YOUR WATCH FACE
+ *
+ * and a description of how use it
+ *
+ */
+
+typedef struct {
+    uint8_t hundredths;
+    uint8_t tenths;
+    uint8_t ones;
+    uint8_t tens;
+    uint8_t hundreds;
+    uint8_t thousands;
+} calculator_number_t;
+
+typedef enum {
+    PLACEHOLDER_HUNDREDTHS,
+    PLACEHOLDER_TENTHS,
+    PLACEHOLDER_ONES,
+    PLACEHOLDER_TENS,
+    PLACEHOLDER_HUNDREDS,
+    PLACEHOLDER_THOUSANDS
+} calculator_placeholder_t;
+
+typedef enum {
+    OP_ADD,
+    OP_SUB,
+    OP_MULT,
+    OP_DIV,
+    OP_ROOT,
+    OP_POWER,
+} calculator_operation_t;
+
+typedef enum {
+    MODE_ENTERING_FIRST_NUM,
+    MODE_CHOOSING,
+    MODE_ENTERING_SECOND_NUM,
+    MODE_VIEW_RESULTS
+} calculator_mode_t;
+
+typedef struct {
+    calculator_number_t first_num;
+    calculator_number_t second_num;
+    calculator_number_t result;
+    calculator_operation_t operation;
+    calculator_mode_t mode;
+    calculator_placeholder_t placeholder;
+} simple_calculator_state_t;
+
+void simple_calculator_face_setup(movement_settings_t *settings, uint8_t watch_face_index, void ** context_ptr);
+void simple_calculator_face_activate(movement_settings_t *settings, void *context);
+bool simple_calculator_face_loop(movement_event_t event, movement_settings_t *settings, void *context);
+void simple_calculator_face_resign(movement_settings_t *settings, void *context);
+
+#define simple_calculator_face ((const watch_face_t){ \
+    simple_calculator_face_setup, \
+    simple_calculator_face_activate, \
+    simple_calculator_face_loop, \
+    simple_calculator_face_resign, \
+    NULL, \
+})
+
+#endif // SIMPLE_CALCULATOR_FACE_H_
+
