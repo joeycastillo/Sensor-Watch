b344b19302d314a0a367ab02868eed8ec9b4af55
Metehan <99metehanarslan@gmail.com>
2024-09-01 02:33:05 +0300

    add layla tune

--- movement/movement_custom_signal_tunes.h
+++ movement/movement_custom_signal_tunes.h
@@ -100,4 +100,21 @@ int8_t signal_tune[] = {
 };
 #endif // SIGNAL_TUNE_POWER_RANGERS
 
+#ifdef SIGNAL_TUNE_LAYLA
+int8_t signal_tune[] = {
+    BUZZER_NOTE_A6, 4,
+    BUZZER_NOTE_REST, 1,
+    BUZZER_NOTE_C7, 5,  
+    BUZZER_NOTE_REST, 2,
+    BUZZER_NOTE_D7, 5,
+    BUZZER_NOTE_REST, 1,
+    BUZZER_NOTE_F7, 5,
+    BUZZER_NOTE_REST, 1,
+    BUZZER_NOTE_D7, 5,
+    BUZZER_NOTE_REST, 2,
+    BUZZER_NOTE_D7, 15,
+    0
+};
+#endif // SIGNAL_TUNE_LAYLA
+
 #endif // MOVEMENT_CUSTOM_SIGNAL_TUNES_H_
