90e02f7dda697891450a7af6bfd4c4d5ea09c166
joeycastillo <joeycastillo@utexas.edu>
2024-09-17 20:51:12 -0400

    silence warnings in menstrual_cycle_face

--- movement/watch_faces/complication/menstrual_cycle_face.c
+++ movement/watch_faces/complication/menstrual_cycle_face.c
@@ -252,6 +252,7 @@ static inline void update_shortest_longest_cycle(menstrual_cycle_state_t *state)
 
 void menstrual_cycle_face_setup(movement_settings_t *settings, uint8_t watch_face_index, void ** context_ptr) {
     (void) watch_face_index;
+    (void) settings;
     
     if (*context_ptr == NULL) {
         *context_ptr = malloc(sizeof(menstrual_cycle_state_t));
@@ -412,7 +413,7 @@ bool menstrual_cycle_face_loop(movement_event_t event, movement_settings_t *sett
     if (state->dates.reg)
         watch_set_indicator(WATCH_INDICATOR_SIGNAL); // signal that we are now in a tracking state
 
-    char buf[11];
+    char buf[13];
     switch (current_page) {
         case period_in_num_days:
             sprintf(buf, "%2d", days_till_period(state));
