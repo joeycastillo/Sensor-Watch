57772f003a3eb58e3e9d758c4ce776bda085fdd3
David Volovskiy <devolov@gmail.com>
2024-09-21 09:41:08 -0400

    Minutes in makefile print display correctly

--- make.mk
+++ make.mk
@@ -277,7 +277,7 @@ CFLAGS += -DINITIAL_MONTH=$(CURRENT_MONTH)
 CFLAGS += -DINITIAL_DAY=$(CURRENT_DAY)
 CFLAGS += -DINITIAL_HOUR=$(CURRENT_HOUR)
 CFLAGS += -DINITIAL_MINUTE=$(CURRENT_MINUTE)
-$(info Default time set to $(CURRENT_HOUR):$(CURRENT_MINUTE) on $(shell date +"%b") $(CURRENT_DAY) $(shell date +"%Y") $(shell date +%Z))
+$(info Default time set to $(CURRENT_HOUR):$(shell printf "%02d" $(CURRENT_MINUTE)) on $(shell date +"%b") $(CURRENT_DAY) $(shell date +"%Y") $(shell date +%Z))
 else
 $(error DATE must be YEAR, DAY, or MIN if used.)
 endif
